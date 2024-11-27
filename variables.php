ab73f337d41b2ce4fe551d1e00234bcf78f38b5c
Kim Raya <kimraya1017@gmail.com>
2024-11-27 08:41:20 +0800

    This is my progress for the crash course

---
+++ variables.php
@@ -0,0 +1,8 @@
+<?php
+    $fullName = 'Kim L. Raya'; // String
+    $age = 25;  // Integer
+    $isMale = true; // Boolean  
+    $height = 1.7;  // Float
+    $salary = null; // Null
+    echo $fullName;
+?>
